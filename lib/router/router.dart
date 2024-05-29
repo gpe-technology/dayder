9c733f4f898356647ed14fe92efff44f14d8ee80
alphasow <alphaibrahima.sow@outlook.fr>
2024-05-29 22:43:02 +0200

    auth navigate guard

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:dayder/presentation/authentication/bloc/authentication_bloc.dart';
 import 'package:flutter/material.dart';
